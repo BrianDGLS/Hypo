f0ff854b1c812dc95e59b5d7930ff2241b44b265
BrianDGLS <briandgls92@gmail.com>
2018-06-25 23:31:33 +0100

    feature: added test coverage

--- lib/hypo.ts
+++ lib/hypo.ts
@@ -3,8 +3,8 @@ class ContainerService {
 
   constructor(
     protected container: Container,
-    protected serviceCreationCallback: (container: Container) => any) {
-  }
+    protected serviceCreationCallback: (container: Container) => any
+  ) {}
 
   get(): any {
     if (this.serviceInstance) {
@@ -32,8 +32,8 @@ class ProtectedParameter {
 
   constructor(
     protected container: Container,
-    protected parameterCreationCallback: (container: Container) => any) {
-  }
+    protected parameterCreationCallback: (container: Container) => any
+  ) {}
 
   get(): any {
     if (!this.initialised) {
@@ -52,23 +52,32 @@ export class Container {
 
   register(
     service: string | Container,
-    serviceCreationCallback?: ((container: Container) => any)
+    serviceCreationCallback?:
+      | ((container: Container) => any)
       | FactoryContainerService
       | ProtectedParameter
   ): Container {
-    if (typeof service === 'string' && serviceCreationCallback) {
+    if (typeof service === 'string') {
+      this.throwIfServiceAlreadyRegistered(service)
+      this.throwIfServiceCreationCallbackNotSupplied(serviceCreationCallback)
+
       if (serviceCreationCallback instanceof FactoryContainerService) {
         this.registry.set(service, serviceCreationCallback)
       } else if (serviceCreationCallback instanceof ProtectedParameter) {
+        this.throwIfParameterAlreadyRegistered(service)
+
         Object.assign(this, {
-          get [(service as string)]() {
+          get [service as string]() {
             return (serviceCreationCallback as any).get()
           }
         })
       } else {
-        this.registry.set(service, new ContainerService(this, serviceCreationCallback))
+        this.registry.set(
+          service,
+          new ContainerService(this, serviceCreationCallback as any)
+        )
       }
-    } else if(service instanceof Container) {
+    } else if (service instanceof Container) {
       service.registry.forEach((value, key) => {
         this.register(key, value.raw())
       })
@@ -77,31 +86,36 @@ export class Container {
     return this
   }
 
-  factory(serviceCreationCallback: (container: Container) => any): FactoryContainerService {
+  factory(
+    serviceCreationCallback: (container: Container) => any
+  ): FactoryContainerService {
     return new FactoryContainerService(this, serviceCreationCallback)
   }
 
-  protect(parameterCreationCallback: (container: Container) => any): ProtectedParameter {
+  protect(
+    parameterCreationCallback: (container: Container) => any
+  ): ProtectedParameter {
     return new ProtectedParameter(this, parameterCreationCallback)
   }
 
   get(service: string): any {
-    if (this.registry.has(service)) {
-      return this.registry.get(service).get()
-    }
+    this.throwIfNotInRegistry(service)
+    return this.registry.get(service).get()
   }
 
-  extend(service: string, extensionCallback: (storage: any, container: Container) => any): any {
-    if (this.registry.has(service)) {
-      const serviceMethod = this.get(service)
-      extensionCallback(serviceMethod, this)
-    }
+  extend(
+    service: string,
+    extensionCallback: (storage: any, container: Container) => any
+  ): any {
+    this.throwIfNotInRegistry(service)
+
+    const serviceMethod = this.get(service)
+    extensionCallback(serviceMethod, this)
   }
 
   raw(service: string): any {
-    if (this.registry.has(service)) {
-      return this.registry.get(service).raw()
-    }
+    this.throwIfNotInRegistry(service)
+    return this.registry.get(service).raw()
   }
 
   delete(service: string): void {
@@ -112,5 +126,35 @@ export class Container {
 
   deleteAll(): void {
     this.registry.clear()
+  }
+
+  private throwIfServiceAlreadyRegistered(service: string): void {
+    if (this.registry.has(service)) {
+      throw new Error(
+        `${service} has already been registered, user unique service name`
+      )
+    }
+  }
+
+  private throwIfParameterAlreadyRegistered(parameter: string): void {
+    if (this.hasOwnProperty(parameter)) {
+      throw new Error(
+        `${parameter} has already been registered, user unique parameter name`
+      )
+    }
+  }
+
+  private throwIfServiceCreationCallbackNotSupplied(callback: any): void {
+    if (!callback) {
+      throw new Error(
+        `callback must be supplied when registering a service or protected parameter`
+      )
+    }
+  }
+
+  private throwIfNotInRegistry(service: string): void {
+    if (!this.registry.has(service)) {
+      throw new Error(`${service} not in registry`)
+    }
   }
 }
